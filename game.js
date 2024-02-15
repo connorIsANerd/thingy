345162266a1e12d2d81a848d9d41ddb6cfe39584
connorIsANerd <156211261+connorIsANerd@users.noreply.github.com>
2024-02-15 12:25:20 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,19 @@
 let leftPressed = false;
 let rightPressed = false;
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 const image = new Image();
+image.src = 'babananan.gif';
+const x = 100;
+const y = 100;
 const messageDiv = document.getElementById('message');
 document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
 function keyDownHandler(event) {
     if (event.key === 'ArrowLeft') {
         leftPressed = true;
+        ctx.drawImage(image, x, y);
       messageDiv.textContent = "Left arrow key pressed!";
-        image.src = 'babananan.gif';
     } else if (event.key === 'ArrowRight') {
         rightPressed = true;
     }
